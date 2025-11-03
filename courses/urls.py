d2667619d0b0fc3c2a2a03cbf04980c34cf0a4de
gonulpolat <gp29polat@gmail.com>
2025-11-03 08:39:01 +0300

    Sayfadaki kurs başlıkları linke çevrildi

--- courses/urls.py
+++ courses/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<course_id>/', views.details),
+    path('<course_id>/', views.details, name="course_details"),
     path('kategori/<int:category_id>/', views.get_courses_by_category_id),
     path('kategori/<str:category_name>/', views.get_courses_by_category_name, name="courses_by_category_name"),
 ]
