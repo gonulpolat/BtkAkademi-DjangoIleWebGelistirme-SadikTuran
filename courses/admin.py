3857eff23b90c662edcc7f119774035555cb4c24
gonulpolat <gp29polat@gmail.com>
2025-11-03 15:54:59 +0300

    Many to many relations
    Course ile Category arasındaki 1 to many ilişkisi değiştirildi
    
    Böylelikle course tablosundan category_id alanı kaldırıldı ve 2 tablo (course, category) için kesişim tablosu oluşturuldu

--- courses/admin.py
+++ courses/admin.py
@@ -5,11 +5,11 @@ from .models import Category, Course
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'category', 'isActive')
+    list_display = ('title', 'slug', 'isActive')
     list_display_links = ('title', 'slug')
-    prepopulated_fields = {'slug': ('title',),}    # model üzerindeki save() methodunu kaldırdın. Add Course sayfasında slug alanı görünüyor, sen title bilgisini oluşturdukça bu alan da otomatik yazılıyor, farkı ise artık slug alanını sen de değiştirebilirsin
-    list_filter = ('isActive', 'category')
-    list_editable = ('isActive',) # Change course sayfasına gitmeden isActive alanı değiştirilebilir
+    prepopulated_fields = {'slug': ('title',),}
+    list_filter = ('isActive',)
+    list_editable = ('isActive',)
     search_fields = ('title', 'description')
 
 
--- courses/models.py
+++ courses/models.py
@@ -15,21 +15,10 @@ class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     imageUrl = models.CharField(max_length=50)
-    date = models.DateField(auto_now=True)  # Admin paneli Add Course'ta date alanı görünmez, tabi Course Change'de de görünmez
+    date = models.DateField(auto_now=True)
     isActive = models.BooleanField()
-    """
-        blank = True   : admin panelindeki formda slug alanı boş bırakılabilir.
-        editable=False : zaten slug alanı save methodu ile kaydediliyor, sen slug alanına herhangi bir değer yazsan da kendi otomatik değer veriyor. bu nedenle slug alanına yazı yazılamasın. kurs ekleme panelinde artık slug alanı görünmez.
-    """
     slug = models.SlugField(default="",blank=True, null=False, unique=True, db_index=True)
-    """
-        CASCADE     : Bir kategori silinirse onunla ilgili kurs da silinir
-        SET_NULL    : Bir kategori silinirse kurstaki category_id kolonuna null değeri atanır. bunu kullanmak için null=True demen gerekli, çünkü category kolonu varsayılan olarak null kabul etmiyor
-        SET_DEFAULT : Bir kategori silindiğinde kurstaki category_id kolonuna default bir değer atanır. bunun için de default parametresini tanımlaman gerekli.
-        * Database'de alanın adı category_id olarak gitse de shell üzerinde işlem yaparken category olarak kullanılır.
-    """
-    category = models.ForeignKey(Category, default=2, on_delete=models.CASCADE, related_name="kurslar") # default parametresinin sebebi veri tabanında kayıtların olması
-
+    categories = models.ManyToManyField(Category)
 
     def __str__(self):
         return f"{self.title}"
