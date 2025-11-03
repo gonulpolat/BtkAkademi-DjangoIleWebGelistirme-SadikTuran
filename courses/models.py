c06621fcd1997690fd985449938db4c936ef4da4
gonulpolat <gp29polat@gmail.com>
2025-11-03 11:56:13 +0300

    Course modelindeki date alanı güncellendi

--- courses/models.py
+++ courses/models.py
@@ -15,7 +15,7 @@ class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     imageUrl = models.CharField(max_length=50)
-    date = models.DateField()
+    date = models.DateField(auto_now=True)  # Admin paneli Add Course'ta date alanı görünmez, tabi Course Change'de de görünmez
     isActive = models.BooleanField()
     """
         blank = True   : admin panelindeki formda slug alanı boş bırakılabilir.
@@ -26,6 +26,7 @@ class Course(models.Model):
         CASCADE     : Bir kategori silinirse onunla ilgili kurs da silinir
         SET_NULL    : Bir kategori silinirse kurstaki category_id kolonuna null değeri atanır. bunu kullanmak için null=True demen gerekli, çünkü category kolonu varsayılan olarak null kabul etmiyor
         SET_DEFAULT : Bir kategori silindiğinde kurstaki category_id kolonuna default bir değer atanır. bunun için de default parametresini tanımlaman gerekli.
+        * Database'de alanın adı category_id olarak gitse de shell üzerinde işlem yaparken category olarak kullanılır.
     """
     category = models.ForeignKey(Category, default=2, on_delete=models.CASCADE) # default parametresinin sebebi veri tabanında kayıtların olması
 
