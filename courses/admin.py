85d633c706230a6b5ab7736c16a86b0378b0045b
gonulpolat <gp29polat@gmail.com>
2025-11-03 10:56:03 +0300

    Admin paneli özelleştirildi

--- courses/admin.py
+++ courses/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Category, Course
 
 
-admin.site.register(Course)
-admin.site.register(Category)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'isActive')
+    list_display_links = ('title', 'slug')
+    readonly_fields = ('slug',)  # slug alanı editable olmadığı için Change course sayfasında da görünmüyordu, artık görünür
+    list_filter = ('isActive',)
+    list_editable = ('isActive',) # Change course sayfasına gitmeden isActive alanı değiştirilebilir
+    search_fields = ('title', 'description')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
