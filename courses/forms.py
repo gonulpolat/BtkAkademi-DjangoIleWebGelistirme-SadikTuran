dff7b2fbb4c4067245ed8da1b471227761d9c351
gonulpolat <gp29polat@gmail.com>
2025-11-04 21:39:18 +0300

    ModelForm - 1
    CourseCreateForm Form yerine ModelForm üzerinden oluşturuldu
    
    Yani tek tek oluşturmak yerine zaten Course modelindeki alanları kullanıldı

--- courses/forms.py
+++ courses/forms.py
@@ -1,34 +1,10 @@
 from django import forms
 
+from .models import Course
 
-class CourseCreateForm(forms.Form):
-    title = forms.CharField(
-        error_messages={
-            'required': 'Başlık zorunlu bir alandır.'
-        },
-        widget=forms.TextInput(attrs={'class':'form-control'})
-        )
-    # label'a class özelliği atayabiliyorsun (course-create.html sayfasında)
-    # fakat input'a ekleyemiyorsun
-    # onu da burda widget üzerinden ekliyorsun. zaten Charfield'in varsayılan widget özelliği TextInput. class özelliği eklemek için yazılıyor.    
 
-    description = forms.CharField(
-        error_messages={
-            'required': 'Açıklama zorunlu bir alandır.'
-        },
-        widget=forms.Textarea(attrs={'class':'form-control'})
-    )
-
-    imageUrl = forms.CharField(
-        error_messages={
-            'required': 'Resim zorunlu bir alandır.'
-        },
-        widget=forms.TextInput(attrs={'class':'form-control'})
-    )
-
-    slug = forms.SlugField(
-        error_messages={
-            'required': 'Slug zorunlu bir alandır.'
-        },
-        widget=forms.TextInput(attrs={'class':'form-control'})
-    )
+class CourseCreateForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        # fields = ('__all__')
+        fields = ('title', 'description', 'imageUrl', 'slug')
