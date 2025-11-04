e68137da161eb85e1fbdd7fe79a6485601787c3a
gonulpolat <gp29polat@gmail.com>
2025-11-04 21:05:33 +0300

    Form Templates
    form direkt sayfaya gönderilmek yerine html etiketleri ile gönderildi

--- courses/forms.py
+++ courses/forms.py
@@ -2,15 +2,33 @@ from django import forms
 
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField(label='Başlık', error_messages={
-        'required': 'Başlık zorunlu bir alandır.'
-    })     
-    description = forms.CharField(widget=forms.Textarea, label='Açıklama', error_messages={
-        'required': 'Açıklama zorunlu bir alandır.'
-    })
-    imageUrl = forms.CharField(label='Resim Url', error_messages={
-        'required': 'Resim zorunlu bir alandır.'
-    })
-    slug = forms.SlugField(error_messages={
-        'required': 'Slug zorunlu bir alandır.'
-    })
+    title = forms.CharField(
+        error_messages={
+            'required': 'Başlık zorunlu bir alandır.'
+        },
+        widget=forms.TextInput(attrs={'class':'form-control'})
+        )
+    # label'a class özelliği atayabiliyorsun (course-create.html sayfasında)
+    # fakat input'a ekleyemiyorsun
+    # onu da burda widget üzerinden ekliyorsun. zaten Charfield'in varsayılan widget özelliği TextInput. class özelliği eklemek için yazılıyor.    
+
+    description = forms.CharField(
+        error_messages={
+            'required': 'Açıklama zorunlu bir alandır.'
+        },
+        widget=forms.Textarea(attrs={'class':'form-control'})
+    )
+
+    imageUrl = forms.CharField(
+        error_messages={
+            'required': 'Resim zorunlu bir alandır.'
+        },
+        widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+
+    slug = forms.SlugField(
+        error_messages={
+            'required': 'Slug zorunlu bir alandır.'
+        },
+        widget=forms.TextInput(attrs={'class':'form-control'})
+    )
