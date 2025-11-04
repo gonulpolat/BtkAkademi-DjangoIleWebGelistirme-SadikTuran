afbb3888c0c0c86d0a1ffdcb2daf5a4adcefd599
gonulpolat <gp29polat@gmail.com>
2025-11-04 20:25:19 +0300

    Form Properties
    Kurs Ekle form alanlarına özellikler eklendi

--- courses/forms.py
+++ courses/forms.py
@@ -2,7 +2,15 @@ from django import forms
 
 
 class CourseCreateForm(forms.Form):
-    title = forms.CharField()       # hepsi için default required = True
-    description = forms.CharField(widget=forms.Textarea)
-    imageUrl = forms.CharField()
-    slug = forms.SlugField()
+    title = forms.CharField(label='Başlık', error_messages={
+        'required': 'Başlık zorunlu bir alandır.'
+    })     
+    description = forms.CharField(widget=forms.Textarea, label='Açıklama', error_messages={
+        'required': 'Açıklama zorunlu bir alandır.'
+    })
+    imageUrl = forms.CharField(label='Resim Url', error_messages={
+        'required': 'Resim zorunlu bir alandır.'
+    })
+    slug = forms.SlugField(error_messages={
+        'required': 'Slug zorunlu bir alandır.'
+    })
