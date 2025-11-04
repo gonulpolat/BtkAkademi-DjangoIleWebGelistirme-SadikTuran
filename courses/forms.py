50a4c19e7debfa39734c51f589d2f0a9171b29f4
gonulpolat <gp29polat@gmail.com>
2025-11-04 21:47:58 +0300

    ModelForm -2
    Görünüm düzenlendi

--- courses/forms.py
+++ courses/forms.py
@@ -6,5 +6,17 @@ from .models import Course
 class CourseCreateForm(forms.ModelForm):
     class Meta:
         model = Course
-        # fields = ('__all__')
+        # fields = '__all__'   (az önce yanlış yazmışım parantez olmayacak sorry)
         fields = ('title', 'description', 'imageUrl', 'slug')
+        labels = {
+            'title': 'Başlık',
+            'description': 'Açıklama',
+            'imageUrl': 'Resim Url',
+        }
+        widgets = {
+            'title' : forms.TextInput(attrs={'class':'form-control'}),
+            'description' : forms.Textarea(attrs={'class':'form-control'}),
+            'imageUrl' : forms.TextInput(attrs={'class':'form-control'}),
+            'slug' : forms.TextInput(attrs={'class':'form-control'}),
+        }
+        # widgets eklenme sebebi class özelliği eklemek
