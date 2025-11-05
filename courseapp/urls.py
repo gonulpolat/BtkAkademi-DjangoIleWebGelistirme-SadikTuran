a55410a96782a843a5cd5c6a392f6b09a2fc54b7
gonulpolat <gp29polat@gmail.com>
2025-11-05 16:54:36 +0300

    Login, register, logout url'leri, sayfaları ve yönlendirme fonksiyonları oluşturuldu

--- courseapp/urls.py
+++ courseapp/urls.py
@@ -6,5 +6,6 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('pages.urls')),
     path('kurs/', include('courses.urls')),
+    path('account/', include('account.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
