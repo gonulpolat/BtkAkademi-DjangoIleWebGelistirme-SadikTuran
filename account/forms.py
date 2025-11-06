4abd0ff86f7424aea72cbad7953d8afcd3f5a60e
gonulpolat <gp29polat@gmail.com>
2025-11-06 11:39:10 +0300

    Giriş işlemi için AuthenticationForm özelleştirildi

---
+++ account/forms.py
@@ -0,0 +1,21 @@
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms import ValidationError, widgets
+
+
+class LoginUserForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget = widgets.TextInput(attrs={'class':'form-control',})
+        self.fields['password'].widget = widgets.PasswordInput(attrs={'class':'form-control',})
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if username == 'admin':
+            messages.add_message(self.request, messages.INFO, 'Hoş geldin, admin!')
+        return username
+    
+    def confirm_login_allowed(self, user):
+        # sadece öğrenmek amacıyla oluşturulan bir fonksiyon
+        if user.username.startswith('b'):
+            raise ValidationError('Bu kullanıcı ile giriş yapılamaz.')
--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
+
+from .forms import LoginUserForm
 
 
 def user_login(request):
@@ -10,7 +11,7 @@ def user_login(request):
         messages.add_message(request, messages.ERROR, 'Yetkiniz yok!.')
         return render(request, 'account/login.html')
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = LoginUserForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -28,7 +29,7 @@ def user_login(request):
         else:
             return render(request, 'account/login.html', {'form': form})
     else:
-        form = AuthenticationForm()
+        form = LoginUserForm()
         return render(request, 'account/login.html', {'form': form})
 
 def user_register(request):
